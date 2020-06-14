e946cda3d585f0edabc1caa7e15b204ec727e491
dfastje <dfastje@gmail.com>
2020-06-14 17:51:55 -0400

    cleanup and adding activemq base

---
+++ activemq-project/src/test/java/me/dfastje/emptyproject/EmptyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dfastje.emptyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
